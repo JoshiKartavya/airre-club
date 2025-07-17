0ac0710824504cbc2e65f53d1812fea90dc5d094
DeltaGamerz <deltagamerz2712@gmail.com>
2025-07-17 20:18:26 +0530

    fix cursor problem

--- src/CustomCursorProvider.tsx
+++ src/CustomCursorProvider.tsx
@@ -30,7 +30,7 @@ export const CustomCursorProvider: React.FC<{ children: React.ReactNode }> = ({ 
       {showCursor && (
         <div
           ref={cursorRef}
-          className="fixed top-0 left-0 w-4 h-4 rounded-full pointer-events-none z-50 mix-blend-difference hidden md:block"
+          className="fixed top-0 left-0 w-4 h-4 rounded-full pointer-events-none z-90 mix-blend-difference hidden md:block"
           style={{ backgroundColor: "white" }}
         />
       )}
